ce295b40882ac67f6837cacca7bffa675417c4a2
hope664 <umuhozahope5@gmail.com>
2026-05-16 21:25:32 +0200

    added the navbar

---
+++ src/components/Navbar/NavBar.jsx
@@ -0,0 +1,52 @@
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+
+      {/* Logo */}
+      <div className="navbar__logo">
+        <span className="navbar__logo-icon">🍽️</span>
+        <span className="navbar__logo-text">Eataly</span>
+      </div>
+
+      {/* Center links */}
+      <ul className="navbar__links">
+        <li className="navbar__link navbar__link--active">Explore</li>
+        <li className="navbar__link">Restaurants</li>
+        <li className="navbar__link">Market</li>
+        <li className="navbar__link">Classes</li>
+      </ul>
+
+      {/* Right side */}
+      <div className="navbar__right">
+        {/* Search */}
+        <div className="navbar__search">
+          <span className="navbar__search-icon">🔍</span>
+          <input
+            type="text"
+            placeholder="Search experiences, ingredients, recipes..."
+          />
+        </div>
+
+        {/* Notification icon */}
+        <button className="navbar__icon-btn">🔔</button>
+
+        {/* Cart icon */}
+        <button className="navbar__icon-btn">🛒</button>
+
+        {/* User */}
+        <div className="navbar__user">
+          <div className="navbar__user-info">
+            <span className="navbar__user-name">Marco Rossi</span>
+            <span className="navbar__user-badge">Gold Member</span>
+          </div>
+          <div className="navbar__avatar">MR</div>
+        </div>
+      </div>
+
+    </nav>
+  )
+}
+
+export default Navbar
