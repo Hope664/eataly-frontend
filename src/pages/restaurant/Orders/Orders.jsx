10ee91fa8961e9792d3f376fb5644cd7a586968c
hope664 <umuhozahope5@gmail.com>
2026-06-04 14:10:00 +0200

    dealing with bookings

--- src/pages/restaurant/Orders/Orders.jsx
+++ src/pages/restaurant/Orders/Orders.jsx
@@ -53,7 +53,7 @@ const Orders = () => {
 
         <div className="ro-sidebar__bottom">
           <Link to="/settings"   className="ro-nav-item"><span>⚙️</span> Settings</Link>
-          <Link to="/support" className="ro-nav-item"><span>👤</span>Support</Link>
+          <Link to="/support" className="ro-nav-item"><span>💬</span>Support</Link>
           <div className="ro-sidebar__user">
             <div className="ro-sidebar__avatar">JD</div>
             <div>
